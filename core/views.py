eded8e2ccbb30b246ab9382ba403cff203754536
Дмитрий Михасько <dzmitry.mikhasko@MacBook-Air-Dmitrij.local>
2025-09-02 21:55:57 +0300

    create basic front

--- core/views.py
+++ core/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Tets Title ',
-        'user': "Test User",
-        }
-    return render(request, 'index.html', context)
+        'title': 'Flatmates Hub',
+        'message': 'Добро пожаловать!'
+    }
+    return render(request, 'pages/index.html', context)
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
--- flatmates/urls.py
+++ flatmates/urls.py
@@ -20,4 +20,6 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
