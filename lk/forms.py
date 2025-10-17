e07f68f70b234b4aba0abaaba53b6fe55f39c778
weeecap <dzmitry.mikhasko@gmail.com>
2025-10-17 14:44:15 +0300

    complete registration logic was added

--- lk/forms.py
+++ lk/forms.py
@@ -5,7 +5,10 @@ from users.models import User
 
 
 class UserProfileForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Имя пользователя')
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'readonly':True}), label='Email')
+    tg = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Telegram')
 
     class Meta:
-        model = User 
+        model = User
         fields = ('username', 'email', 'tg', 'image')
